0c55c529d53e7109f8162c9027788be02f7df879
mutale <mutalechanda016@gmail.com>
2024-11-03 20:10:36 +0200

    minor refactoring

--- src/main/java/com/codewithmut/demo/student/studentService.java
+++ src/main/java/com/codewithmut/demo/student/studentService.java
@@ -2,11 +2,8 @@ package com.codewithmut.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class studentService {
